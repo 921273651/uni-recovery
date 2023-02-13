cb0f4d564f0d8e6e73d4ec86a7a3930f88b8c80b
wenx777 <1197686371@qq.com>
2023-02-13 12:32:54 +0800

    feat：更改api地址为线上服务器ip；

--- service/request.js
+++ service/request.js
@@ -1,7 +1,8 @@
 export default {
 	config: {
 		// baseURL: 'http://192.168.31.63:3000/api/',
-		baseURL: 'http://localhost:3000/api/',
+		// baseURL: 'http://localhost:3000/api/',
+		baseURL: 'http://39.107.241.37:8808/api/',
 		// 处理token的方法
 		getToken() {
 			let token = uni.getStorageSync('token');
